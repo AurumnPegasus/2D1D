ebdb4fc6c31bf19c77847b65a7ae7b1b1e154633
Aakash-AA <aakash.aanegola@gmail.com>
2020-10-01 20:59:17 +0530

    Added functionality for display in PokeBase.py

--- PokeBase.py
+++ PokeBase.py
@@ -2,30 +2,57 @@ import subprocess as sp
 import pymysql
 import pymysql.cursors
 
-"""def DispPokemon():
-	tmp = sp.call('clear', shell=True)
-	print("Select display condition: ")
-	print("1. Type")
-	print("2. Tier")
-	print("3. Generation")
-	print("4. Total Base Stats")
-	print("5. Single stat and type")
-	ch = int(input("Enter your choice : "))
-	if ch == 1:
-    	#
-	elif ch == 2:
-		#
-	elif ch == 3:
-		#
-	elif ch == 4:
-		#
-	elif ch == 5:
-	else:
-		print("Invalid option")
+def DispPokemon():
+	try:
+		tmp = sp.call('clear', shell=True)
+		print("Select display condition: ")
+		print("1. Type")
+		print("2. Tier")
+		print("3. Generation")
+		print("4. Total Base Stats")
+		print("5. Single stat and type")
+		ch = int(input("Enter your choice : "))
+		if ch == 1:
+			print("Valid types are :\nNormal\t\tFighting\tBug\nDark\t\tDragon\t\tElectric\nFairy\t\tFiret\t\tFlying\nGhost\t\tGrass\t\tGround\nIce\t\tPoison\t\tPsychic\nRock\t\tSteel\t\tWater\n")
+			type = input("Enter type (First letter capitalized ex: Fire): ")
+			query = "SELECT P.PokedexID, N.Name, P.Generation, P.Tier, P.EvolvesFrom, T.Type1, T.Type2 FROM POKEMON AS P, POKENAME AS N, POKETYPE AS T WHERE P.PokedexID = N.PokedexID AND P.PokedexID = T.PokedexID AND (T.Type1 = '%s' OR T.Type2 = '%s')" % (type, type)
+			cur.execute(query)
+			rows = cur.fetchall()
+			print("PokedexID	Name	Generation	Tier	Evolves From	Type")
+			for row in rows:	
+				print(row['P.PokedexID'], row['N.Name'], row['P.Generation'], row['P.Tier'], row['P.EvolvesFrom'], row['T.Type1'], row['T.Type2'])
+			
+		elif ch == 2:
+			print("Valid Tiers are are : OU\tRU\tNU\tUBER")
+			tier = input("Enter Tier: ")
+			query = "SELECT P.PokedexID, N.Name, P.Generation, P.Tier, P.EvolvesFrom, T.Type1, T.Type2 FROM POKEMON AS P, POKENAME AS N, POKETYPE AS T WHERE P.PokedexID = N.PokedexID AND P.PokedexID = T.PokedexID AND P.Tier = '%s'" % (tier)
+			cur.execute(query)
+			rows = cur.fetchall()
+			print("PokedexID	Name	Generation	Tier	Evolves From	Type")
+			for row in rows:	
+				print(row['P.PokedexID'], row['N.Name'], row['P.Generation'], row['P.Tier'], row['P.EvolvesFrom'], row['T.Type1'], row['T.Type2'])
 
+		elif ch == 3:
+			print("Valid Generations are are : (1, 2, 3, 4, 5, 6, 7, 8)")
+			generation = int(input("Enter Generation: "))
+			query = "SELECT P.PokedexID, N.Name, P.Generation, P.Tier, P.EvolvesFrom, T.Type1, T.Type2 FROM POKEMON AS P, POKENAME AS N, POKETYPE AS T WHERE P.PokedexID = N.PokedexID AND P.PokedexID = T.PokedexID AND P.Generation = '%d'" % (generation)
+			cur.execute(query)
+			rows = cur.fetchall()
+			print("PokedexID	Name	Generation	Tier	Evolves From	Type")
+			for row in rows:	
+				print(row['P.PokedexID'], row['N.Name'], row['P.Generation'], row['P.Tier'], row['P.EvolvesFrom'], row['T.Type1'], row['T.Type2'])
+		elif ch == 4:
+			print()
+		elif ch == 5:
+			print()
+		else:
+			print("Invalid option")
+	except:
+		print("Error in displaying pokemon")
 	getch = input("Press ENTER to return")
 
 
+"""
 def SearchPokemon():
 	tmp = sp.call('clear', shell=True)
 	print("Select search condition : ")
@@ -69,7 +96,6 @@ def CheckEffect():
 	getch = input("Press ENTER to return")
 
 def DispChampion():
-	
 	getch = input("Press ENTER to return")
 
 def AddChampion():
@@ -80,56 +106,63 @@ def EditChampion():
 
 def RemoveChampion():
 	getch = input("Press ENTER to return")
+"""
 
 def DispLegendary():
+	query = "SELECT P.PokedexID, P.Name, L.Lore FROM POKENAME AS P, LEGENDARY AS L WHERE P.PokedexID = L.PokedexID"
+	cur.execute(query)
+	rows = cur.fetchall()
+	print("PokedexID	Name	Lore")
+	for row in rows:
+		print(row['P.PokedexID'], row['P.Name'], row['L.Lore'])
 	getch = input("Press ENTER to return")
-"""
+
 
 # Main loop
 while(1):
-    tmp = sp.call('clear', shell=True)
+	tmp = sp.call('clear', shell=True)
     
     # Can be skipped if you want to hard core username and password
-    username = input("Username: ")
-    password = input("Password: ")
+	username = input("Username: ")
+	password = input("Password: ")
 
-    try:
+	try:
         # Set db name accordingly which have been create by you
         # Set host to the server's address if you don't want to use local SQL server 
-        con = pymysql.connect(host='localhost',
-                              user=username,
-                              password=password,
-                              db='POKEBASE',
-                              cursorclass=pymysql.cursors.DictCursor)
-        tmp = sp.call('clear', shell=True)
+		con = pymysql.connect(host='localhost',
+							  user=username,
+							  password=password,
+							  db='POKEBASE',
+							  cursorclass=pymysql.cursors.DictCursor)
+		tmp = sp.call('clear', shell=True)
+		
+		if(con.open):
+			print("Connected")
+		else:
+			print("Failed to connect")
 
-        if(con.open):
-            print("Connected")
-        else:
-            print("Failed to connect")
+		tmp = input("Press ENTER to continue")
 
-        tmp = input("Press ENTER to continue")
-
-        with con.cursor() as cur:
-            while(1):
-                tmp = sp.call('clear', shell=True)
+		with con.cursor() as cur:
+			while(1):
+				tmp = sp.call('clear', shell=True)
                	
-                print("Welcome to the PokeBase!")
-                print("Please enter your choice :")
-                print("1. Display pokemon")
-                print("2. Search pokemon")
-                print("3. Search moves")
-                print("4. Check effectiveness")
-                print("5. Display champion builds")
-                print("6. Add champion build")
-                print("7. Edit champion build")
-                print("8. Delete a champion build")
-                print("9. Display legendary pokemon")
-                print("10. Exit")
-                ch = int(input("Enter your choice : "))
-                tmp = sp.call('clear', shell=True)
-				#if ch == 1:
-                #	DispPokemon()
+				print("Welcome to the PokeBase!")
+				print("Please enter your choice :")
+				print("1. Display pokemon")
+				print("2. Search pokemon")
+				print("3. Search moves")
+				print("4. Check effectiveness")
+				print("5. Display champion builds")
+				print("6. Add champion build")
+				print("7. Edit champion build")
+				print("8. Delete a champion build")
+				print("9. Display legendary pokemon")
+				print("10. Exit")
+				ch = int(input("Enter your choice : "))
+				tmp = sp.call('clear', shell=True)
+				if ch == 1:
+					DispPokemon()
                 #elif ch == 2:
                 #	SearchPokemon()
                 #elif ch == 3:
@@ -144,14 +177,14 @@ while(1):
                 #	EditChampion()
                 #elif ch == 8:
                 #	RemoveChampion()
-                #elif ch == 9:
-                #	DispLegendary()
-               	#elif ch == 10:
-               	#	break
-                #else:
-                #    tmp = input("Enter any key to CONTINUE>")
+				elif ch == 9:
+					DispLegendary()
+				elif ch == 10:
+					break
+				else:
+					tmp = input("Enter any key to CONTINUE>")
 				
-    except:
-        tmp = sp.call('clear', shell=True)
-        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
-        tmp = input("Enter any key to CONTINUE>")
+	except:
+		tmp = sp.call('clear', shell=True)
+		print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+		tmp = input("Enter any key to CONTINUE>")
